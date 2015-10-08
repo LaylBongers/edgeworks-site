0a04a83dee0920ab1efe87dee58bc507ac85bff6
Layl <layl@edgeworksgames.com>
2015-10-08 18:27:29 +0200

    Workaround for a problem with hyper timeouts

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 #[macro_use] extern crate nickel;
+extern crate hyper;
 
-use nickel::Nickel;
+use nickel::{Nickel};
+use hyper::header::{Connection};
 
 fn main() {
     let mut server = Nickel::new();
 
     server.utilize(router! {
-        get "**" => |_req, _res| {
+        get "**" => |_req, mut res| {
+            res.headers_mut().set(Connection::close());
             "IT'S ALIIIIIIVE!"
         }
     });
-    
+
     server.listen("0.0.0.0:80");
 }
