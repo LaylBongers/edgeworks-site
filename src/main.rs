30f2102004618b2e87e87aefd7b32ea7cd4870b9
Layl <layl@edgeworksgames.com>
2015-11-03 15:37:35 +0100

    Port to iron

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-#[macro_use] extern crate nickel;
-extern crate hyper;
+extern crate iron;
 
-use nickel::{Nickel};
-use hyper::header::{Connection};
+use iron::prelude::*;
+use iron::status;
 
 fn main() {
-    let mut server = Nickel::new();
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "IT'S ALIIIIIVE!")))
+    }
 
-    server.utilize(router! {
-        get "**" => |_req, mut res| {
-            res.headers_mut().set(Connection::close());
-            "IT'S ALIIIIIIVE!"
-        }
-    });
-
-    server.listen("0.0.0.0:80");
+    Iron::new(hello_world).http("0.0.0.0:80").unwrap();
 }
